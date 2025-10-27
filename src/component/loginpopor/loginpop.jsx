93547b51b0711e78c165446da75690d34406b419
ridwan <ridwanullahgbolagade@gmail.com>
2025-10-27 13:43:23 +0100

    add

--- src/component/loginpopor/loginpop.jsx
+++ src/component/loginpopor/loginpop.jsx
@@ -7,74 +7,114 @@ import axios from "axios"
 import { useNavigate } from 'react-router-dom'
 
 
-const LoginPopup = ({setShowlogin}) => {
-     const [currState,setCurrState] = useState("login")
-     const {url,setToken} = useContext(StoreContext)
-     const [data,setData] = useState({
+const LoginPopup = ({ setShowlogin }) => {
+    const [currState, setCurrState] = useState("login")
+    const [passwordVisible, setPasswordVisible] = useState(false); // New state
+
+    const { url, setToken } = useContext(StoreContext)
+    const [data, setData] = useState({
         name: "",
         email: "",
-        password:""
-     })
+        password: ""
+    })
 
-     const onChangeHandler = (e) =>{
+    const onChangeHandler = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-        setData(data=>({...data,[name]:value}))
-     }
+        setData(data => ({ ...data, [name]: value }))
+    }
 
-     const onLogin = async (e) => {
+    const onLogin = async (e) => {
         e.preventDefault()
         let newUrl = url;
         if (currState === "login") {
             newUrl += "/api/user/login"
-        }else{
+        } else {
             newUrl += "/api/user/register"
         }
 
         console.log(newUrl);
-        
-        const res = await axios.post(newUrl,data);
+
+        const res = await axios.post(newUrl, data);
         if (res.data.success) {
             setToken(res.data.token);
-            localStorage.setItem("token",res.data.token)
+            localStorage.setItem("token", res.data.token)
             setShowlogin(false)
             alert(res.data.message)
         }
-        else{
+        else {
             alert(res.data.message)
         }
-     }
-     const navigate = useNavigate()
-     return (
-    <div className="loginPopup">
-        <form className="login_popup_cotainar" onSubmit={onLogin}>
-            <div className="login_popup_title">
-                <h2>{currState}</h2>
-                <img onClick={()=>setShowlogin(false)} src={assets.cross_icon} alt="" />
-            </div>
-            <div className="login_popup_inputs">
-                {currState==="login"? <></> : <input type="text" name="name" value={data.name} onChange={onChangeHandler} placeholder='Your name' required />}
-                <input type="email" name="email" value={data.email} onChange={onChangeHandler} placeholder='example@gmail.com' required />
-                <input type="password" name="password" value={data.password} onChange={onChangeHandler} placeholder='Pssword' required />
-                <button type='submit' >{currState==="Sign Up"? "Create account": "login"}</button>
-                <div className="passcon">
-                    <p>forget you passwor : <span onClick={()=>{navigate('/forgot-password');setShowlogin(false)}}>click here to reset</span></p>
+    }
+
+
+    const togglePasswordVisibility = (e) => {
+        e.preventDefault();
+        setPasswordVisible(prev => !prev);
+    }
+
+    const navigate = useNavigate()
+    return (
+        <div className="loginPopup">
+            <form className="login_popup_cotainar" onSubmit={onLogin}>
+                <div className="login_popup_title">
+                    <h2>{currState}</h2>
+                    <img onClick={() => setShowlogin(false)} src={assets.cross_icon} alt="" />
                 </div>
-                <div className="login_popup_condition" >
-                    <input type="Checkbox" required/>
-                    <p onClick={()=>{navigate('privacyPolicy');setShowlogin(false)}}>By continuing, i agree to the terms of use & privacy policy.</p>
+                <div className="login_popup_inputs">
+                    {currState === "login" ? <></> : <input type="text" name="name" value={data.name} onChange={onChangeHandler} placeholder='Your name' required />}
+                    <input type="email" name="email" value={data.email} onChange={onChangeHandler} placeholder='example@gmail.com' required />
+
+                    <div className="login_popup_input showpasscon">
+
+                        <input
+                            type={passwordVisible ? "text" : "password"}
+                            name="password"
+                            id="passinp"
+                            value={data.password}
+                            onChange={onChangeHandler}
+                            placeholder='Password'
+                            required
+                        />
+
+                        <button
+                            className="passshowbtn"
+                            type="button"
+                            onClick={togglePasswordVisibility}
+                        >
+                            {passwordVisible ? "Hide" : "Show"}
+                        </button>
+                    </div>
+
+                    <button type='submit' >{currState === "Sign Up" ? "Create account" : "login"}</button>
+
+
                 </div>
-            </div>
-            {
-                currState==="login"
-                ?<p>Create a new accout? <span onClick={()=>{setCurrState("Sign Up")}}>Click here</span></p>
-                :
-            <p>Already have an accout? <span onClick={()=>{setCurrState("login")}}>Login here</span></p>
-            }
-            
-        </form>
-    </div>
-  )
+
+                {
+                    currState === "login"
+                        ?
+                        <div>
+                            <div className="passcon">
+                                <p>forget your password: <span onClick={() => { navigate('/forgot-password'); setShowlogin(false) }}>click here to reset</span></p>
+                            </div>
+                            <p>Create a new account? <span onClick={() => { setCurrState("Sign Up") }}>Click here</span></p>
+                        </div>
+                        :
+                        <div>
+                            <div className="login_popup_condition" >
+                                <label htmlFor="check">
+                                    <input type="Checkbox" id="check" required />
+                                    <p>By continuing, i agree to the <span onClick={() => { navigate('privacyPolicy'); setShowlogin(false) }}>terms of use & privacy policy.</span> </p>
+                                </label>
+                            </div>
+                            <p>Already have an account? <span onClick={() => { setCurrState("login") }}>Login here</span></p>
+                        </div>
+                }
+
+            </form>
+        </div>
+    )
 }
 
 
--- src/component/navBar/navBar.jsx
+++ src/component/navBar/navBar.jsx
@@ -6,15 +6,26 @@ import { useContext } from "react"
 import { StoreContext } from "../../contexts/storeContexts"
 
 
-const NavBar = ({setShowlogin}) => {
+const NavBar = ({ setShowlogin }) => {
 
 
-  const [menu,setMenu] = useState('')
+  const [menu, setMenu] = useState('')
 
-  const {getTotalCartAmount, token,setToken} = useContext(StoreContext)
+  const { getTotalCartAmount, token, setToken } = useContext(StoreContext)
+
+  function toggleSearchInput() {
+    const container = document.getElementById('search-container');
+    const input = document.getElementById('search-input');
+
+    container.classList.toggle('open');
+
+    if (container.classList.contains('open')) {
+      input.focus();
+    }
+  }
 
   const navigate = useNavigate()
-  const logOut = ()=>{
+  const logOut = () => {
     localStorage.removeItem("token");
     setToken("");
     navigate("/")
@@ -25,27 +36,41 @@ const NavBar = ({setShowlogin}) => {
     <div className="navBar">
       <Link to="/"><img src={assets.logo01} className="logo" alt="" /></Link>
       <ul className="navbar_menu">
-        <Link to="/" onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
-        <a href="#ExploreMenu" onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</a>
-        <a href="#appdownload" onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-app</a>
-        <a href="#footer" onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>About</a>
+        <Link to="/" onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</Link>
+        <a href="#ExploreMenu" onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</a>
+        <a href="#appdownload" onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile-app</a>
+        <a href="#footer" onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>About</a>
       </ul>
       <div className="navbar-right">
-        <img src={assets.search_icon} alt="" />
+        <div class="search-container" id="search-container">
+          <input
+            type="text"
+            placeholder="What are you craving?"
+            class="search-input"
+            id="search-input"
+            aria-label="Search food or restaurants"
+          />
+          
+            <img src={assets.search_icon}
+            class="search-icon-btn"
+            onClick={toggleSearchInput}
+             alt="" />
+        </div>
+        
         <div className="navbar-search-icon">
           <Link to="/cart"><img src={assets.basket_icon} alt="" /></Link>
-          <div className={getTotalCartAmount()===0?'':'dot'}></div>
+          <div className={getTotalCartAmount() === 0 ? '' : 'dot'}></div>
         </div>
         {!token ?
-          <button onClick={()=>setShowlogin(true)}>sign in</button>:
+          <button onClick={() => setShowlogin(true)}>sign in</button> :
           <div className="navbar_profile">
             <img src={assets.profile_icon} alt="" />
             <ul className="nav_profile_dropDown">
-              <li onClick={()=>navigate('/myorders')}><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+              <li onClick={() => navigate('/myorders')}><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
               <hr />
               <li onClick={logOut}><img src={assets.logout_icon} alt="" /><p>logOut</p></li>
             </ul>
-            </div>}
+          </div>}
 
       </div>
     </div>
